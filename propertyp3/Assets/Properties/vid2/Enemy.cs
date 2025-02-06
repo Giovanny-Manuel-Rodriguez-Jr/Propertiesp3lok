4a69856391acd5fddd19cb60f7794b195766c01d
Giovanny Rodriguez Jr <145072297+Giovanny-Manuel-Rodriguez-Jr@users.noreply.github.com>
2025-02-06 11:09:38 -0800

    done wit both

---
+++ propertyp3/Assets/Properties/vid2/Enemy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public static int enemyCount = 0;
+    // Start is called before the first frame update
+    public Enemy()
+    {
+        enemyCount++;
+    }
+}
