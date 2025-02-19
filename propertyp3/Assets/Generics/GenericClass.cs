2a91dec0bd8b1102fec11e4c5166d6a1b53c3ac4
Giovanny Rodriguez Jr <145072297+Giovanny-Manuel-Rodriguez-Jr@users.noreply.github.com>
2025-02-19 11:05:40 -0800

    done wi6t generics

---
+++ propertyp3/Assets/Generics/GenericClass.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericClass <T>
+{
+    
+        T item;
+
+        public void UpdateItem(T newItem)
+        {
+          = ne  item wItem;
+        }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
